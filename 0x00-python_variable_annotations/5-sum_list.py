e7829ffb50cff1de24732fa85fa7ce3987fff27d
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-01-06 10:03:04 +0100

    sum list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Contains the function sum_list """
+import typing
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """Sums up two floats and returns """
     res: float = 0.0
     for i in input_list:
