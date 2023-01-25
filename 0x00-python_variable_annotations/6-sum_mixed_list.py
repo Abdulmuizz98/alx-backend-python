33afbb1a3d30b8e3d23fec9d0597ad03de2607c1
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-01-25 01:19:58 +0100

    sum mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 import typing
 
 
-def sum_mixed_list(mxd_lst: typing.Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
     """sums the items of a list of integers and floats """
     res: float = 0.0
     for i in mxd_lst:
