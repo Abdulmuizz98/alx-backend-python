619f4add9a8747a94ec1956721466bdf4d677903
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-03 22:47:13 +0100

    task 102

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,6 +5,7 @@ from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Zooms an array, not like I know how to break zoom """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
