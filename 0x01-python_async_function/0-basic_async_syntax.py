c603a24fd3137ea45aeacaf1636eb226262e5dab
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-03-28 12:02:46 +0100

    feat: implement wait_random - to wait for random delay

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Contains the function wait_random
+"""
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """ Async wait for a random delay from 0 - max_delay
+    """
+    time = random.random() * (max_delay - 0) + 0
+    await asyncio.sleep(time)
