2b8a74282b54995df214c0c76477843a3f000d06
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-03-28 12:08:37 +0100

    feat: implement wait_random - to wait for random delay

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,9 +5,10 @@
 
 import asyncio
 import random
+from typing import Union
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: Union[int, float] = 10) -> float:
     """ Async wait for a random delay from 0 - max_delay
     """
     time = random.random() * (max_delay - 0) + 0
